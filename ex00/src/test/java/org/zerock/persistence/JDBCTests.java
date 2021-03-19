6b986c7a3f945bc046f66770b71ab192fd2dba60
jiwon <ji_ji1997@naver.com>
2021-03-19 19:42:40 +0900

    ch3 finish + subject finish
    3chapter 강의 다듣고 과제 끝!

---
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,36 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.apache.commons.logging.Log;
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch( Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try(Connection con =
+				DriverManager.getConnection(
+						"jdbc:oracle:thin:@localhost:1521:XE",
+						"BOOK_EX",
+						"1111"
+						)) {
+			log.info(con);
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
