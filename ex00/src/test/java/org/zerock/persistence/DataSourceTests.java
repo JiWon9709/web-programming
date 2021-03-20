debd527d2c61c5973e29d6c0d1cac7c138340ac7
jiwon <ji_ji1997@naver.com>
2021-03-21 03:30:39 +0900

    3week finish & ~ch4.2

--- ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,11 +25,24 @@ public class DataSourceTests {
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
 	
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
 	@Test
 	public void testConnection() {
 		try (Connection con = dataSource.getConnection()) {
 			log.info(con);
 		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testMyBatis() {
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
