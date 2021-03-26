5b57da1986bc48304b91cf4914d4921221fa65db
jiwon <ji_ji1997@naver.com>
2021-03-27 03:59:35 +0900

    ch4 finish

--- ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -22,4 +22,10 @@ public class TimeMapperTests {
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime());
 	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+	}
 }
