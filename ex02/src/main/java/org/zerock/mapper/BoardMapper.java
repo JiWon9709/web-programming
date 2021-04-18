ab489051688cca630a753f1b4c52256ba7a29385
jiwon <ji_ji1997@naver.com>
2021-04-19 00:03:04 +0900

    ch8 finish

---
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,20 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	//@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board); // 저장만
+	public void insertSelectKey(BoardVO board); // 저장 + 기본키
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
