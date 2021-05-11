c231d8c6a746f9ce998b9491fc0b0a158246086a
jiwon <ji_ji1997@naver.com>
2021-05-11 20:18:22 +0900

    ch10 finish and ch 11 studying

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -34,4 +34,38 @@ public class BoardControllerTests {
 	public void testList() throws Exception {
 		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
 	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 내용")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "5")).andReturn().getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "1")
+				.param("title", "수정된 테스트 새글 제목")
+				.param("content", "수정된 테스트 새글 내용")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "25"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
 }
